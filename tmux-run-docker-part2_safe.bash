30e4b78b83e3ff6115688174d7e8039e059b5c4e
rtjk <47841774+rtjk@users.noreply.github.com>
2023-11-05 17:49:32 +0100

    AAA

--- tmux-run-docker-part2_safe.bash
+++ tmux-run-docker-part2_safe.bash
@@ -1,7 +1,8 @@
 #! /bin/bash
 #tmux select-pane -t docker-bind:0.1
-tmux send-keys -t docker-bind:0.1 'dig +timeout=11 +tries=1 test'$1'.goertzen' Enter
-sleep 10
-tmux capture-pane -t docker-bind:0.1 -pS - > dig_logs/run_$1.log
-tmux send-keys -t docker-bind:0.1 -R Enter
-tmux clear-history -t docker-bind:0.1
+#tmux send-keys -t docker-bind:0.1 'dig +timeout=11 +tries=1 test'$1'.goertzen' Enter
+#sleep 10
+#tmux capture-pane -t docker-bind:0.1 -pS - > dig_logs/run_$1.log
+#tmux send-keys -t docker-bind:0.1 -R Enter
+#tmux clear-history -t docker-bind:0.1
+echo "xxxxxxxxxxxxxx ERROR xxxxxxxxxxxxxx"
